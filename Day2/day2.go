836273911fd0e1514c9be1176846b252d16f5881
Martin <mb4079@student.uni-lj.si>
2020-12-05 11:51:18 +0100

    Added challenges

---
+++ Day2/day2.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func main() {
+	inputs := readInput()
+	fmt.Println(calculateValidPasswords(inputs))
+	fmt.Println(calculateValidPasswords2(inputs))
+}
+
+func readInput() []string {
+	file, err := os.Open("input.txt")
+	checkError(err)
+
+	scanner := bufio.NewScanner(file)
+	var inputs []string
+
+	for scanner.Scan() {
+		inputs = append(inputs, scanner.Text())
+	}
+
+	return inputs
+}
+
+func calculateValidPasswords(passwords []string) int {
+	var validPasswords = 0
+
+	re := regexp.MustCompile(" ")
+	reNum := regexp.MustCompile("-")
+
+	for i := range passwords {
+		split := re.Split(passwords[i], -1)
+		// get values
+		limits := split[0]
+		num1, err := strconv.Atoi(reNum.Split(limits, -1)[0])
+		num2, err := strconv.Atoi(reNum.Split(limits, -1)[1])
+		checkError(err)
+		word := split[1][:len(split[1])-1]
+
+		count := strings.Count(split[2], word)
+
+		if num1 <= count && count <= num2 {
+			validPasswords++
+		}
+
+	}
+	return validPasswords
+}
+
+func calculateValidPasswords2(passwords []string) int {
+	var validPasswords = 0
+
+	re := regexp.MustCompile(" ")
+	reNum := regexp.MustCompile("-")
+
+	for i := range passwords {
+		split := re.Split(passwords[i], -1)
+		// get values
+		limits := split[0]
+		pos1, err := strconv.Atoi(reNum.Split(limits, -1)[0])
+		pos2, err := strconv.Atoi(reNum.Split(limits, -1)[1])
+		checkError(err)
+		word := split[1][:len(split[1])-1]
+
+		test := split[2]
+
+		if ((string(test[pos1-1]) == word) || (string(test[pos2-1]) == word)) && !((string(test[pos1-1]) == word) && (string(test[pos2-1]) == word)) {
+			validPasswords++
+		}
+
+	}
+	return validPasswords
+}
---
+++ Day3/day3.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func main() {
+	treeMap := readinput()
+
+	var down = []int{1, 1, 1, 1, 2}
+	var right = []int{1, 3, 5, 7, 1}
+	var numberOfTrees []int
+	for index := 0; index < len(down); index++ {
+		var trees = 0
+		var limit = 0
+		for i := down[index]; i < len(treeMap); i += down[index] {
+			limit = (limit + right[index]) % len(treeMap[0])
+
+			fmt.Println(i, limit)
+
+			if treeMap[i][limit] == '#' {
+				trees++
+			}
+		}
+
+		numberOfTrees = append(numberOfTrees, trees)
+
+	}
+	count := 1
+	for i := range numberOfTrees {
+		count *= numberOfTrees[i]
+		fmt.Println(numberOfTrees[i])
+	}
+	fmt.Println(numberOfTrees)
+	fmt.Println(count)
+
+}
+
+func readinput() [][]byte {
+	file, err := os.Open("input.txt")
+	checkError(err)
+	var treemap [][]byte
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := []byte(scanner.Text())
+		treemap = append(treemap, line)
+	}
+
+	return treemap
+}
