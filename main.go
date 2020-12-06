4815a7f56f1a5c08fabbcd37598a88de5b2e63de
Martin <mb4079@student.uni-lj.si>
2020-12-06 12:28:17 +0100

    fixed module tree

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 
 	day5 "github.com/Mberga14/Advent_of_code_2020/Day5"
+	day6 "github.com/Mberga14/Advent_of_code_2020/Day6"
 )
 
 func main() {
 	fmt.Printf("Solution for 5th day:\n\tPart 1: %d\n\tPart 2: %d\n", day5.GetMaxSeatID(), day5.FindMissingSeatID())
-	fmt.Println("Solution for day6 is: ", day6.parseInput())
+	fmt.Println("Solution for day6 is: ", day6.ParseInput())
 }
